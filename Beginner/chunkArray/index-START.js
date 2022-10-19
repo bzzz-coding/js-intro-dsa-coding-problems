191f5126238ecdc3cdd99a126c670ca19078a2ed
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-10-19 11:10:40 -0700

    Completed chunkArray

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -4,7 +4,7 @@
  E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
-// for loop
+// for loop - FASTER
 function chunkArray1(array, size) {
     let result = []
     for (let i = 0; i < array.length; i += size) {
@@ -14,7 +14,17 @@ function chunkArray1(array, size) {
     return result
 }
 
+// recursion
+function chunkArray2(array, size) {
+    if (array.length <= size) {
+        return [array]
+    }
+    return [array.slice(0,size), ...chunkArray2(array.slice(size), size)]
+}
 
-console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5))
+
+// console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5))
+
+// Comparing: https://jsbench.me/cql9fy926r/1
 
 module.exports = chunkArray
